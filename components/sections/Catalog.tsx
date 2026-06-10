36f07a859428cf172c6c0392689e735a8eb0de78
Renato Martins <108689676+renatomart15@users.noreply.github.com>
2026-06-10 13:33:58 -0300

    fix: correção de semântica do html no catalog

--- components/sections/Catalog.tsx
+++ components/sections/Catalog.tsx
@@ -5,8 +5,14 @@ const Catalog = () => {
   return (
     <section>
       <div className="px-5 py-3">
+<<<<<<< Updated upstream
         <p className="text-vermelho font-body font-semibold text-xs">
           NOSSO CATÁLOGO
+=======
+        <p className="text-vermelho font-body font-semibold text-sm flex gap-2 items-center">
+          <span>NOSSO CATÁLOGO</span>
+          <span className="h-1 bg-vermelho rounded-4xl flex-1 md:hidden"></span>
+>>>>>>> Stashed changes
         </p>
         <h1 className="font-heading text-2xl font-extrabold">
           Canecas para cada momento
