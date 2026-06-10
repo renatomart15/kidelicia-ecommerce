4c7984a906ad0ea9e723f992ff0e0df59941245b
Renato Martins <108689676+renatomart15@users.noreply.github.com>
2026-06-10 11:20:19 -0300

    feat: carrossel responsivo na seção de catálogo

--- components/sections/Catalog.tsx
+++ components/sections/Catalog.tsx
@@ -1,14 +1,26 @@
-import React from 'react'
-import ProductCard from '../ui/ProductCard'
+import React from "react";
+import ProductCard from "../ui/ProductCard";
 
 const Catalog = () => {
   return (
-    <section className='px-5 py-3'>
-        <p className='text-vermelho font-body font-semibold text-xs'>NOSSO CATÁLOGO</p>
-        <h1 className='font-heading text-2xl font-bold'>Canecas para cada momento</h1>
-        <ProductCard/>
+    <section>
+      <div className="px-5 py-3">
+        <p className="text-vermelho font-body font-semibold text-xs">
+          NOSSO CATÁLOGO
+        </p>
+        <h1 className="font-heading text-2xl font-extrabold">
+          Canecas para cada momento
+        </h1>
+      </div>
+      <div className="flex px-8 scroll-pl-17 gap-8 overflow-x-scroll scroll-smooth snap-x snap-mandatory [&::-webkit-scrollbar]:hidden">
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Catalog
+export default Catalog;
--- components/ui/ProductCard.tsx
+++ components/ui/ProductCard.tsx
@@ -1,6 +1,9 @@
 const ProductCard = () => {
   return (
-    <div className="rounded-sm shadow overflow-hidden w-60 my-5">
+    <div
+      className="rounded-sm shadow overflow-hidden w-60 my-5 snap-start snap-always shrink-0
+"
+    >
       <div className="flex justify-center items-center h-52 bg-red-100">
         <p className="text-vermelho font-semibold text-xl">Caneca Clássica</p>
       </div>
@@ -8,10 +11,12 @@ const ProductCard = () => {
         <p>Caneca Clássica Branca</p>
         <div>
           <div className="flex gap-1 mt-1">
-              <p className="text-vermelho">R$ 49,90</p>
-              <p className="text-gray-400 text-xs font-medium pt-0.5">R$ 69,90</p>
+            <p className="text-vermelho">R$ 49,90</p>
+            <p className="text-gray-400 text-xs font-medium pt-0.5">R$ 69,90</p>
           </div>
-          <p className="text-xs font-medium text-gray-600 mt-1">ou 3x sem juros</p>
+          <p className="text-xs font-medium text-gray-600 mt-1">
+            ou 3x sem juros
+          </p>
         </div>
       </div>
     </div>
